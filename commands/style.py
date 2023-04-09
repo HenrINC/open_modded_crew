71260511507c369f3fa69f8f31fc86405f4ed584
henri.questiaux <henri.questiaux@gmail.com>
2023-04-09 17:38:07 +0200

    Style now stored in it's own file

--- commands/style.py
+++ commands/style.py
@@ -1,6 +1,7 @@
 from commands.lazy_command import LazyCommand
 from common import CrewMember, fix_style_property_value
 from base64 import b32decode
+import json
 
 class Command(LazyCommand):
 
@@ -17,46 +18,55 @@ class Command(LazyCommand):
         self.parser.add_argument("--base32", action="store_true")
     
     def _run(self, namespace, member:CrewMember):
+        style = self.crew.get_style()
+        
+        if "style" not in self.crew.service_config["style"]: 
+            self.crew.service_config["style"]["style"] = self.style_to_style_config(style)
+
+        style_config = self.crew.service_config["style"]["style"]
+
         if namespace.value is not None:
             if namespace.base32:
                 namespace.value = b32decode(value, casefold=True).decode()
             namespace.value = fix_style_property_value(namespace.value)
         if namespace.set or namespace.add or namespace.get:
-            style = self.crew.get_style()
             if namespace.property is None:
-                old_cfg = self.crew.service_config["style_service_config"].copy()
+                old_cfg = style_config.copy()
                 if namespace.get:
                     return f"[SUCCESS] {old_cfg}"
-                if namespace.set:
+                elif namespace.set:
                     new_style_cfg = self.style_to_style_config(style)
+                elif namespace.add:
+                    new_style_cfg = self.add_style_to_style_config(style_config, style)
                 else:
-                    if "style_service_config" not in self.crew.service_config:
-                        raise ValueError("Style is not configured, please run \"/style -set\"")
-                    new_style_cfg = self.add_style_to_style_config(self.crew.service_config["style_service_config"], style)
-                self.crew.service_config["style_service_config"] = new_style_cfg
+                    raise ValueError("Namespace asynchronously modified")
+                style_config.update(new_style_cfg)
+                self.crew.save_config()
                 return f"[SUCCESS] {old_cfg} => {new_style_cfg}"
 
             else:
-                if "style_service_config" not in self.crew.service_config:
-                    raise ValueError("Style is not configured, please run \"/style -set\"")
                 if namespace.property not in style:
-                    raise ValueError(f"The property \"{namespace.property}\" is not a style property,\
-                                     available properties are : {list(self.crew.service_config['style_service_config'].keys())}")
+                    raise ValueError(
+                        f"The property \"{namespace.property}\" is not a style property,"
+                        f"available properties are : {list(style_config.keys())}")
                 
                 if namespace.value is None:
                     value = style[namespace.property]
                 else:
                     value = namespace.value
-                old_value = self.crew.service_config["style_service_config"][namespace.property].copy()
+                old_value = style_config[namespace.property].copy()
                 if namespace.get:
                     return f"[SUCCESS]{namespace.property} : {old_value}"
                 
-                if namespace.set:
-                    self.crew.service_config["style_service_config"][namespace.property] = [value]
+                elif namespace.set:
+                    style_config[namespace.property] = [value]
+                elif namespace.add:
+                    style_config[namespace.property].append(value)
                 else:
-                    self.crew.service_config["style_service_config"][namespace.property].append(value)
+                    raise ValueError("Namespace asynchronously modified")
+                self.crew.save_config()
                 return f"[SUCCESS]{namespace.property} : {old_value} => \
-                    {self.crew.service_config['style_service_config'][namespace.property]}"                    
+                    {style_config[namespace.property]}"                    
                 
 
         elif namespace.remove:
@@ -66,7 +76,7 @@ class Command(LazyCommand):
                 raise ValueError("Argument \"value\" is required")
         
         elif namespace.set_delay is not None:
-            self.crew.service_config["style_service_delay"] = namespace.set_delay
+            self.crew.service_config["style"]["delay"] = namespace.set_delay
     
         self.crew.save_config()
 
